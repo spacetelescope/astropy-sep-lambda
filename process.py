81b12db7d29c5fc90bc1f4c0f6773eb6a38aa24a
Arfon Smith <arfon@users.noreply.github.com>
2018-04-17 13:08:07 -0400

    Adding process.py file

---
+++ process.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import uuid
+
+libdir = os.path.join(os.getcwd(), 'lib')
+
+FITS_LOCATION = "https://hla.stsci.edu/cgi-bin/ecfproxy?file_id=hag_j004524.96+403851.8_j8hpdcaoq_v01.drizzle.fits"
+
+import warnings
+from astropy.utils.data import CacheMissingWarning
+warnings.simplefilter('ignore', CacheMissingWarning)
+from astropy.io import fits
+
+# Testing imports to make sure dependencies expressed in requirements.txt are installed correctly.
+import numpy as np
+from photutils import datasets
+from skimage import data
+
+def do_science(fits_location):
+    hdul = fits.open(fits_location)
+    print(hdul.info())
+
+def handler(event, context):
+    do_science(FITS_LOCATION)
+
+if __name__ == "__main__":
+    handler('', '')
